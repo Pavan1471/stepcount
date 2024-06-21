14cd6d82965a331a87bde4cfd5d6bf4111c2efee
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2024-06-21 19:38:10 +0530

    modified:   src/StepCounter.jsx

--- src/StepCounter.jsx
+++ src/StepCounter.jsx
@@ -53,7 +53,7 @@ const StepCounter = () => {
 
   return (
     <div>
-      <h1>Ste Count</h1>
+      <h1>St Count</h1>
       <p>Steps: {steps}</p>
       <p>Acceleration Magnitude: {accelerationMagnitude.toFixed(2)}</p>
     </div>
